6a7b10328bc8f4f814e4e07fb7b0417df763d12b
eliasbr26 <elias.ben-rhouma@polytechnique.edu>
2024-11-23 18:04:17 +0100

    other things

--- test_rag/test2.py
+++ test_rag/test2.py
@@ -5,6 +5,8 @@ import faiss
 import os
 from getpass import getpass
 import time
+import pandas as pd 
+
 
 api_key = os.environ["MISTRAL_API_KEY"]
 model = "mistral-small"
@@ -15,30 +17,40 @@ client = Mistral(api_key=api_key)
 import csv
 
 csv_file = 'amazon.csv'
-string_data = ''
+
+df = pd.read_csv(csv_file)
+print(df.columns)
+
+# df = df.head(1000)
 
 
-with open(csv_file, mode='r', newline='', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile)  # Lecture du fichier CSV
-    for row in reader:
-        # Convertir chaque ligne en chaîne, en utilisant un séparateur spécifique (par exemple, une virgule)
-        string_data += ','.join(row) + '\n'  # Séparateur par une virgule, vous pouvez changer cela si nécessaire
+columns_to_embed = ['product_name', 'category', 'about_product', 'review_title', 'review_content']
 
-
-string_data = string_data  # limiter la taille de la chaîne à 1 million de caractères
-chunk_size = 2048
-chunks = [string_data[i:i + chunk_size] for i in range(0, len(string_data), chunk_size)]
-print(len(chunks))
 
 def get_text_embedding(input):
-    time.sleep(0.1)
+    time.sleep(0.25)
     embeddings_batch_response = client.embeddings.create(
           model="mistral-embed",
           inputs=input
       )
     return embeddings_batch_response.data[0].embedding
-text_embeddings = np.array([get_text_embedding(chunk) for chunk in chunks])
 
+# create and store embeddings for each column in the dataframe
+
+for column in columns_to_embed[:1]:
+    print(f'Embedding column {column}')
+    df[column + '_embedding'] = df[column].apply(get_text_embedding)
+
+
+print(df.head())
+
+# save the dataframe to a new csv file
+df.to_csv('amazon_embeddings.csv', index=True)
+
+#text_embeddings = np.array([get_text_embedding(chunk) for chunk in chunks])
+
+#print(text_embeddings)
+"""
 import faiss
 
 d = text_embeddings.shape[1]
@@ -49,32 +61,8 @@ index.add(text_embeddings)
 question = "What type of cable do  you sell?"
 question_embeddings = np.array([get_text_embedding(question)])
 
-
-D, I = index.search(question_embeddings, k=2) # distance, index
-retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
-
-
-prompt = f"""
-Context information is below.
----------------------
-{retrieved_chunk}
----------------------
-Given the context information and not prior knowledge, answer the query.
-Query: {question}
-Answer:
 """
 
 
-def run_mistral(user_message, model="mistral-large-latest"):
-    messages = [
-        {
-            "role": "user", "content": user_message
-        }
-    ]
-    chat_response = client.chat.complete(
-        model=model,
-        messages=messages
-    )
-    return (chat_response.choices[0].message.content)
 
-print(run_mistral(prompt))
+
